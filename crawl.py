43903666bd3fe932ca1700261f44a65f62e9f6e3
Cem Berke Arslan <bscemarslan@gmail.com>
2025-11-29 19:44:59 +0100

    not functional but getting there

--- crawl.py
+++ crawl.py
@@ -1,15 +1,59 @@
 import asyncio
-
-from crawl4ai import AsyncWebCrawler, BrowserConfig, CacheMode, CrawlerRunConfig
-
-
-async def main():
-    browser_conf = BrowserConfig(headless=True)
-    run_conf = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
-
-    async with AsyncWebCrawler(config=browser_conf) as crawler:
-        result = await crawler.arun(url="", config=run_conf)
+import requests
+from crawl4ai import LLMConfig, AsyncWebCrawler, BrowserConfig, CacheMode, CrawlerRunConfig, AdaptiveCrawler, AdaptiveConfig
+import dotenv
+import os
 
 
+dotenv.load_dotenv()
+
+def openrouter_embedding(query):
+    response = requests.get(
+      "https://openrouter.ai/api/v1/embeddings/models",
+      headers={
+        "Authorization": f"Bearer {os.getenv('OPENROUTER_API_KEY')}",
+      }
+    )
+    models = response.json()
+    return models
+    
+    
+    
+ async def subredditcrawl(subreddit_url, num_posts, subject):
+    async with AsyncWebCrawler() as redditcrawler:
+        
+        config = AdaptiveConfig(
+            strategy="embedding",
+            embedding_llm_config=LLMConfig(
+                provider="openrouter/openai/text-embedding-3-small",
+                api_token=os.getenv("OPENROUTER_API_KEY"),
+                temperature=0.7
+            ),
+            n_query_variations=3,
+            embedding_coverage_radius=0.2,
+            confidence_threshold=0.8,
+            max_pages=1,
+            top_k_links=3,
+            min_gain_threshold=0.05
+        )     
+        
+        browser_conf = BrowserConfig(
+                headless=True,
+                verbose=True,
+        
+                # 1. THE PROXY (The Magic Key)
+                proxy=PROXY_URL,
+        
+                # 2. SSL Fix (Equivalent to verify=False)
+                ignore_https_errors=True,
+        
+                # 3. SAVE DATA MODE (Critical for 1GB cap)
+                text_mode=True,   # Tells crawl4ai to skip loading images/media
+                light_mode=True   # Disables some background listeners to save CPU
+            )
+        
+        adaptiveredditcrawl = AdaptiveCrawler(redditcrawler, config)
+        
+    return 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ fastapi_endpoints.py
@@ -0,0 +1,10 @@
+from operator import sub
+from fastapi import FastAPI
+from crawl import subredditcrawl
+
+app = FastAPI()
+
+
+@app.get("/crawlsubreddit")
+async def root(subreddit: str, query: str, num_posts: int=3):
+    return subredditcrawl(subreddit, num_posts, query)
