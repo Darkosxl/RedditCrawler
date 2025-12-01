63a24e686313a3b5f012a4e856062505b12320f6
Cem Berke Arslan <bscemarslan@gmail.com>
2025-12-01 11:11:58 +0100

    dockerfile, authorization to access endpoint

--- fastapi_endpoints.py
+++ fastapi_endpoints.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI, HTTPException
+import os
+from fastapi import FastAPI, HTTPException, Header
 from crawl import subredditcrawl
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+ENDPOINT_PASSWORD = os.getenv("ENDPOINT_PASSWORD")
+
 @app.get("/crawlsubreddit")
 async def root(
-    subreddit: str, 
-    num_posts: int = 3
+    subreddit: str,
+    num_posts: int = 3,
+    authorization: str = Header(None)
 ):
+    if not authorization or authorization != f"Bearer {ENDPOINT_PASSWORD}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     try:
-        data = await subredditcrawl(subreddit, num_posts, query)
+        data = await subredditcrawl(subreddit, num_posts)
         if "error" in data:
             raise HTTPException(status_code=500, detail=data["error"])
-            
+
         return data
 
     except Exception as e:
